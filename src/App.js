7ac677060cfbe2be03629a933f92f709c0b01207
Mazen <mmazen1321@gmail.com>
2023-01-05 20:24:52 +0200

    add overflow hidden to open menu

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import "./App.css";
 import Header from "./components/header";
 import Main from "./components/main";
 import OtherArticles from "./components/other";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [open, setOpen] = useState(false);
-
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (open) {
+      body.style.overflow = "hidden";
+    } else if (!open) {
+      body.style.overflow = "auto";
+    }
+  }, [open]);
   return (
     <div className="App">
       <Header open={open} setOpen={setOpen} />
